ad4d85367dccdd1458eb648bc432133fec5c002b
Emmanuel Egomson <fegomson@gmail.com>
2021-06-17 13:04:51 -0500

    Created a test file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <h2>Hello world!</h2>
+      <h2>hello</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,6 @@
 import React from 'react'
 import App from './App'
+
+test('renders without error', () => {
+  
+})
